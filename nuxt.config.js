616afeab27c47d186d299cb2dd42b26eac8a5e86
LFran <lufra2606@gmail.com>
2017-01-14 15:24:40 +0100

    Update head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   head: {
-    title: 'Auth Routes',
+    title: 'Particle',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', content: 'Auth Routes example' }
+      { hid: 'description', content: 'Control for Photon Particle' }
     ]
   },
   loading: { color: '#3B8070' },
